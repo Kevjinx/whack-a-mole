be0acbfa85b149addf01be671f3d2cb18b663384
Monte <36746765+theflaggship@users.noreply.github.com>
2021-06-03 14:27:07 -0700

    working prototype

--- mole.js
+++ mole.js
@@ -1,13 +1,18 @@
 // Write your JS here.
+window.addEventListener("DOMContentLoaded", (e) => {
+  let rand = Math.ceil(Math.random() * 8)
 
-
-
-document.querySelectorAll(".mole__head").forEach((moleHead)=> {
+  document.querySelectorAll(".mole__head").forEach((moleHead)=> {
+    moleHead.classList.add("digging")
+    document.querySelector("#mole__head" + rand).classList.remove("digging")
     moleHead.addEventListener("click", (e) => {
-        console.log("MONTE PYTHON HOTPINK")
-        console.log(e.target)
         e.target.classList.add("digging")
-        console.log(e.target)
+        e.target.classList.remove("popup")
+        let rand2 = Math.ceil(Math.random() * 8)
+        document.querySelector("#mole__head" + rand2).classList.remove("digging")
+        document.querySelector("#mole__head" + rand2).classList.add("popup")
     })
+  })
 })
 
+//
