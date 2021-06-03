df483b7cd453a7e49b5525eb4c295483b5c34e74
Monte <36746765+theflaggship@users.noreply.github.com>
2021-06-03 10:34:17 -0700

    added event listener to moleheads

--- mole.js
+++ mole.js
@@ -1,1 +1,13 @@
 // Write your JS here.
+
+
+
+document.querySelectorAll(".mole__head").forEach((moleHead)=> {
+    moleHead.addEventListener("click", (e) => {
+        console.log("MONTE PYTHON HOTPINK")
+        console.log(e.target)
+        e.target.classList.add("digging")
+        console.log(e.target)
+    })
+})
+
